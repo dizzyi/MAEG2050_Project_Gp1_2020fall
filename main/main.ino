23428af17cef19a48c099e4538c842436bd55a62
unknown <deeralan827@gmail.com>
2020-10-23 17:11:55 +0800

    renamed

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,15 @@
 #include <can.h>
 #include <SPI.h>
 #include <mcp2515.h>
-#include "Robot_Leg.h"
-#include "MechSpec.h"
+//#include "Robot_Leg.h"
+//#include "MechSpec.h"
 #define MOTOR_SEND_ID 0x200
 #define MOTOR_1_ID 0X201
 #define MOTOR_2_ID 0x202
+
+struct datadump{
+  uint16_t data1;
+};
 
 // boot up CAN bus
 struct can_frame frame;
@@ -14,27 +18,32 @@ MCP2515 mcp2515(10);
 int t = 0;
 
 // boot up Robot_leg IK
-RobotLeg Leg(A_LENGHT,B_LENGHT,C_LENGHT,D_LENGHT,F_LENGHT,ALPHA_ANGLE);
+//RobotLeg Leg(A_LENGHT,B_LENGHT,C_LENGHT,D_LENGHT,F_LENGHT,ALPHA_ANGLE);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  /*pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);*/
+  Serial.begin(9600);
+  /*
   mcp2515.reset();
   mcp2515.setBitrate(CAN_125KBPS);
   mcp2515.setLoopbackMode();
   frame.can_dlc = 8;
+  frame.can_id = MOTOR_1_ID;*/
   Serial.println("------------Initialization Finished ------------");
 }
 
 void loop() {
   // put your main code here, to run repeatedly
   Serial.println("------------New Loop started ------------");
-  Serial.println(t++);
-  frame.can_id = t%2 ? MOTOR_1_ID : MOTOR_2_ID;
+  Serial.println(t);
+  /*frame.can_id = t%2 ? MOTOR_1_ID : MOTOR_2_ID;*/
+  /*
   if (mcp2515.readMessage(&frame) == MCP2515::ERROR_OK) {
     Serial.println("Read Sucessful");
-    Serial.print("Motor ");
-    Serial.println( frame.can_id == MOTOR_1_ID ? "1:" : "2:" );
+    Serial.print("Motor 1 : ");
+    //Serial.println( frame.can_id == MOTOR_1_ID ? "1:" : "2:" );
     Serial.print("Mechanical angle high: ");
     Serial.println(frame.data[0]);
     Serial.print("Mechanical angle low:  ");
@@ -54,7 +63,8 @@ void loop() {
   }
   else{
     Serial.println("Reading Fail");
-  }
-  
-  delay(1000);
+  }*/
+  digitalWrite(13, t%2 ? HIGH: LOW );
+  t++;
+  delay(500);
 }
